728dc2f27050a9add348e12cf752642c44227587
jayamproj-dotcom <jayamproj@gmail.com>
2025-11-24 11:15:00 +0530

    base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lms-nov2/',
+  base: '/lms-nov24/',
   plugins: [react(),],
   css: {
     devSourcemap: true,
